d80563f59a917e063cea5c22aeeabf692270c7ca
Jensy Gomez <contact@jensygomez.us>
2025-11-20 14:38:21 -0300

    lab 002

---
+++ Lab RHEL/002 - lab2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ===================================
+# TechNova - Lab 2 - Verificacion del Sitema
+# Autor: Jensy Gomez
+# Fecha: $(date +%d-%m-%Y)
+
+# Script para documentar el estado inicial del sistema
+
