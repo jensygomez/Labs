b82b894ab7ef24db054c9923b60d532ce4820afd
Jensy Gomez <contact@jensygomez.us>
2025-11-20 17:24:52 -0300

    ok

--- Lab RHEL/002 - lab2.sh
+++ Lab RHEL/002 - lab2.sh
@@ -5,5 +5,29 @@
 # Autor: Jensy Gomez
 # Fecha: $(date +%d-%m-%Y)
 
-# Script para documentar el estado inicial del sistema
 
+
+# ===================================
+# Variables del script
+NOTES="/root/lab2-notes.txt"
+LARGEBINS="/root/lab2-largebins.txt"
+
+# ===================================
+# Definiendo Funciones
+
+run_and_log() {
+    echo -e "\n[+] Ejecutando: $*" | tee -a "$NOTES"
+    echo "=========================================" | tee -a "$NOTES"
+    "$@" 2>&1 | tee -a "$NOTES"
+    echo "" >> "$NOTES"
+}
+
+# ===================================
+# INICIO DEL SCRIPT
+
+# Limpiamos el archivo de notas si ya existe (opcional pero muy limpio en exámenes)
+> "$NOTES"
+
+
+# Objetivo 1.1 - Registrar ruta absoluta actual
+run_and_log pwd
