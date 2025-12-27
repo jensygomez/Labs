db9ca474aa2e8a68eb8c1d100414dfe615f0f29d
Jensy Gomez <contact@jensygomez.us>
2025-12-27 14:17:28 -0300

    ok

--- run_lab.sh
+++ run_lab.sh
@@ -13,7 +13,7 @@ VM_IP="192.168.122.231"
 LAB_ROOT="$HOME/GitHub/Labs/$DOMAIN"
 
 # Selección de variación (puede ser aleatoria o la primera)
-LAB_SCRIPT=$(ls -d "$LAB_ROOT"/"$SLOT"_*/*inject*.sh 2>/dev/null | shuf -n1)
+LAB_SCRIPT=$(find "$LAB_ROOT" -maxdepth 2 -type f -name '*inject*.sh' | shuf -n1)
 
 # Ejecución remota en la VM
 sshpass -p "$VM_PASS" ssh -o StrictHostKeyChecking=no $VM_USER@$VM_IP 'bash -s' < "$LAB_SCRIPT"
