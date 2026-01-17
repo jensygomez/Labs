95afd2eb7152db9a24595d0900adac6d083b435e
jensygomez <contact@jensygomez.us>
2026-01-17 18:29:05 -0300

    ok

--- scenarios/terraform/modules/rocky_vm/main.tf
+++ scenarios/terraform/modules/rocky_vm/main.tf
@@ -2,49 +2,21 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "~> 0.7.6"
+      version = "~> 0.7.6"  # ← Versión actualizada
     }
   }
 }
 
-resource "libvirt_cloudinit_disk" "this" {
-  name      = "${var.vm_name}-cloudinit.iso"
-  user_data = var.cloudinit_user_data
-  meta_data = var.cloudinit_meta_data
+provider "libvirt" {
+  # uri = "qemu:///system"  # ← Comentada o eliminada si ya está en el módulo
 }
 
-resource "libvirt_volume" "root" {
-  name   = "${var.vm_name}-root.qcow2"
-  source = var.base_image
-  format = "qcow2"
-  pool   = "default"
+module "vm" {
+  source = "$ENGINE_DIR/scenarios/terraform/modules/rocky_vm"
+
+  vm_name             = "$VM_NAME"
+  base_image          = "$BASE_IMAGE"
+  cloudinit_user_data = file("$CLOUDINIT_DIR/user-data")
+  cloudinit_meta_data = file("$CLOUDINIT_DIR/meta-data")
 }
-
-resource "libvirt_domain" "this" {
-  name   = var.vm_name
-  memory = 1024
-  vcpu   = 1
-
-  cloudinit = libvirt_cloudinit_disk.this.id
-
-  disk {
-    volume_id = libvirt_volume.root.id
-  }
-
-  network_interface {
-    network_name = "default"
-  }
-
-  autostart = true
-  
-  # CRÍTICO: Usar VNC en lugar de SPICE
-  graphics {
-    type        = "vnc"
-    listen_type = "address"
-    autoport    = true
-  }
-  
-  video {
-    type = "virtio"
-  }
-}
+EOF
