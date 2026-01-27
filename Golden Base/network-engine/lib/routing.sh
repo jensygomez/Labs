366119af52381e0f948025d9593318e8b78d2f4d
jensygomez <contact@jensygomez.us>
2026-01-27 19:54:06 -0300

    ok

---
+++ Golden Base/network-engine/lib/routing.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# ------------------------------------------------------------------------------
+# PRIMITIVA 4: ENSURE ROUTE
+# ------------------------------------------------------------------------------
+ensure_route(){
+  local ns="$1"
+  local dest="$2"
+  local via="$3"
+  if ! ns_exists "$ns"; then
+    echo "❌ Namespace $ns no existe"
+    return 1
+  fi
+  # Normalizar Destino
+  local route_dest
+  if [[ "$dest" == "default" ]]; then 
+    route_dest="default"
+  else  
+    route_dest="$dest"
+  fi
+  # Ruta ya Existe...?
+  if ip netns exec "$ns" ip route show | grep -qw "$route_dest via $via"; then
+    echo  "✔ Ruta $route_dest via $via ya existe en $ns"
+    return 0
+  fi
+  # Agregar Ruta
+  ip netns exec "$ns" ip route add "$route_dest" via "$via"
+  echo "+ Ruta $route_dest via $via agregada en $ns"
+}
