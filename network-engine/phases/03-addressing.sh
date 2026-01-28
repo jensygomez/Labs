5f64b7f3005fb5237a6a8d5b0473028f2f6c0361
jensygomez <contact@jensygomez.us>
2026-01-28 09:37:22 -0300

    ok

--- network-engine/phases/03-addressing.sh
+++ network-engine/phases/03-addressing.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# network-engine/phases/03-addressing.sh
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
+source "$BASE_DIR/lib/idempotency.sh"
 source "$BASE_DIR/lib/netns.sh"
+source "$BASE_DIR/lib/addressing.sh"
 source "$BASE_DIR/topology/lab.conf"
+
 
 run_phase() {
   echo "[FASE 3] IPs"
