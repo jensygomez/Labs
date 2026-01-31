b569881491c84dadac4d4a21540de59be1e13eda
jensygomez <contact@jensygomez.us>
2026-01-30 21:05:09 -0300

    ok

--- network-engine/phases/06-nat.sh
+++ network-engine/phases/06-nat.sh
@@ -11,6 +11,7 @@ run_phase(){
     echo "[FASE 6] Nat & Egress..."
     for nat in "${NAT_ROUTERS[@]}"; do
         IFS=":" read -r ns iface <<< "$nat"
+        echo "  Configurando NAT para $ns en interfaz $iface"
         ensure_nat "$ns" "$iface"
     done
 }
