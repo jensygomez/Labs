61816d028193fcbd9ab3e345805acd0ff9fa8eb0
jensygomez <contact@jensygomez.com>
2026-02-17 23:57:12 -0300

    ok

--- Golden-Base/lab-001.sh
+++ Golden-Base/lab-001.sh
@@ -199,7 +199,7 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 10.0.0.1        core-gw
 EOF
   # 3. Comando de identidad (Hostname en memoria)
-  ip netns exec CORE-GW hostname CORE-GW
+  ip netns exec CORE-GW hostname core-gw
   # --------------------------------
   
   ip netns exec CORE-GW ip link set lo up
