6a276c3448e5637047e52abbdf87462e6a447eb3
jensygomez <contact@jensygomez.com>
2026-02-20 15:11:43 -0300

    ok

--- Golden-Base/lab-001.sh
+++ Golden-Base/lab-001.sh
@@ -225,7 +225,7 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     "apt update::apt update -qq" \
     "dependencias::apt install -y -qq ca-certificates curl gnupg lsb-release" \
     "keyrings::install -m 0755 -d /etc/apt/keyrings" \
-    "GPG key::curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -f -o /etc/apt/keyrings/docker.gpg"
+    "GPG key::curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -f -o /etc/apt/keyrings/docker.gpg" \
     "repositorio Docker::echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null" \
     "repo update::apt update -qq" \
     "docker-ce::apt install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin" \
