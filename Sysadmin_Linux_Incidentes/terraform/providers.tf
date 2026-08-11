186b64173a5006ba68f36f9da3cfbfc6436ebcaf
jensygomez <contact@jensygomez.us>
2026-08-11 15:04:34 -0300

    o

---
+++ Sysadmin_Linux_Incidentes/terraform/providers.tf
@@ -0,0 +1,19 @@
+provider "lxd" {}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3       = "http://10.45.223.1:4566"
+    route53  = "http://10.45.223.1:4566"
+    elbv2    = "http://10.45.223.1:4566"
+    rds      = "http://10.45.223.1:4566"
+    iam      = "http://10.45.223.1:4566"
+  }
+}
