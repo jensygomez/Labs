f9ba7e76697ceb949157f16d92127b5da1b37006
jensygomez <contact@jensygomez.com>
2026-08-26 02:25:28 -0300

    okis

--- SYSTECH-HA-001—Enterprise_Linux_HA_Infrastructure/terraform/variables.tf
+++ SYSTECH-HA-001—Enterprise_Linux_HA_Infrastructure/terraform/variables.tf
@@ -69,6 +69,6 @@ variable "lxc_containers" {
     "dns01"    = { vmid = 121, ip = "10.10.10.20/24", cores = 1, memory = 512, disk_size = 8, role = "dns" }
     "lb01"     = { vmid = 221, ip = "10.10.10.21/24", cores = 1, memory = 512, disk_size = 8, role = "lb" }
     "lb02"     = { vmid = 222, ip = "10.10.10.22/24", cores = 1, memory = 512, disk_size = 8, role = "lb" }
-    "zabbix01" = { vmid = 990, ip = "10.10.10.90/24", cores = 1, memory = 1536, disk_size = 8, role = "monitoring" }
+    
   }
 }
