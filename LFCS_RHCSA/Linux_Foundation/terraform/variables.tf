4b7eb92dbf5dd0242af382c45181059be9c7c13e
jensygomez <contact@jensygomez.com>
2026-09-19 07:19:54 -0300

    okis

---
+++ LFCS_RHCSA/Linux_Foundation/terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "target_node" { type = string; default = "infra" }
+variable "proxmox_host_ip" { type = string; default = "100.93.29.93" }
+
+variable "ssh_public_key" { type = string; sensitive = true }
+variable "ssh_private_key" { type = string; sensitive = true }
+variable "proxmox_password" { type = string; sensitive = true }
